a32eb4a92e959c40858d032a9bc59fa278632d2f
Sudharsan <specsudharsan@gmail.com>
2022-04-21 17:33:27 +0530

    coding challenge done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,10 @@
-let js='amazing';
-if (js==='amazing')alert ('JavaScript is fun');
+let weightMark=prompt("Enter mark weight in kg's");
+let heightMark=prompt("Enter mark height in meters");
+let weightJohn=prompt("Enter John weight in kg's");
+let heightJohn=prompt("Enter John height in meters");
+
+let bmiMark=weightMark/(heightMark**2);
+let bmiJohn=weightJohn/(heightJohn**2);
+
+let markHigherBMI=bmiMark>bmiJohn;
+console.log(markHigherBMI);
