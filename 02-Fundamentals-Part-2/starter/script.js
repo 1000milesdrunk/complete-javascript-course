10c2cbd863eb6423f14121dfab0f1a68d74caf13
Sudharsan <specsudharsan@gmail.com>
2022-04-22 11:54:41 +0530

    challenge 1 done

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,33 @@
+"strict mode"
+//challenge1
+
+const ds1=85;
+const ds2=54;
+const ds3=41;
+
+const ks1=23;
+const ks2=34;
+const ks3=27;
+
+const calcAverage=(s1,s2,s3)=>{
+    return (s1+s2+s3)/3;
+}
+
+const avgDolphins=calcAverage(ds1,ds2,ds3);
+const avgKoalas=calcAverage(ks1,ks2,ks3);
+
+checkWinner(avgDolphins,avgKoalas);
+
+function checkWinner(avgDolphins, avgKoalas){
+    if(avgDolphins/2 >= avgKoalas){
+        console.log(`Dolphins Win`);
+    }else if(avgKoalas/2 >= avgDolphins){
+        console.log(`Koalas Win`);
+    }
+    else{
+        console.log("draw")
+    }
+}
+
+
+
