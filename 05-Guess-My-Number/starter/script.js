91dadcccdf5301d98bdd4e4de73e0373b2c00b7b
Sudharsan <specsudharsan@gmail.com>
2022-04-22 17:46:42 +0530

    building the app

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+//generate a random number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//   console.log(secretNumber);
+document.querySelector('.number').textContent = secretNumber;
+
+//selecting the check button for a click event and writing a function for that event
+document.querySelector('.check').addEventListener('click', function () {
+  //getting the input from user[its String] and converting it to number then storing it in n
+  const n = Number(document.querySelector('.guess').value);
+  //   console.log(n, typeof n);
+
+  //check the condition of secretNumber and n
+  if (secretNumber === n) {
+    document.querySelector('.message').textContent = 'Correct Answer';
+  } else if (n < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low';
+  } else if (n > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High';
+  }
+});
