2873e66a92663cd20482290a9c30c6ef2eb2be62
Sudharsan <specsudharsan@gmail.com>
2022-04-25 15:48:23 +0530

    script started

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -13,12 +13,16 @@ const btnCloseModal = document.querySelector('.close-modal');
 //all the elements with the same class name are as anode list which can be accessed like an array
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 //to use all the three elements
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
+  btnsOpenModal[i].addEventListener('click', openModal);
+  // console.log('Button Clicked');
 
-    /*
+  /*
     here each button is in the for loop and all of them has a eventlistener
     
     classlist is used to display all the classes in the element here modal which contains its element
@@ -27,30 +31,41 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
     no need for the .[dot] notation because we are giving class name as input not as a selector
     
     */
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  // modal.classList.remove('hidden');
+  // overlay.classList.remove('hidden');
 }
 
 //when 'x' button is clicked modal content and the overlay neeeds to be hidden so
 //we are adding hidden class to it again
-btnCloseModal.addEventListener('click', function () {
-  // modal.classList.add('hidden');
-  // overlay.classList.add('hidden');
+btnCloseModal.addEventListener('click', () => closeButton());
+// modal.classList.add('hidden');
+// overlay.classList.add('hidden');
 
-  closeButton();
-});
 //it should exit the content if the overlay is clicked
 
 //but we are writing the same thing twice so better make it a function and call it
-overlay.eventlistener('click', function () {
-  closeButton();
-  // modal.classList.add('hidden');
-  // overlay.classList.add('hidden');
-});
+overlay.addEventListener('click', () => closeButton());
+// modal.classList.add('hidden');
+// overlay.classList.add('hidden');
 
 //creating the function to call for hiding the modal and overlay
 function closeButton() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
+
+//there are different types of events like keyup,keydown,...
+
+//when any key is pressed in keyboard a event object is created by javascript anf the function is called
+//we can give any name as a parameter to function but we give e to say its a event(even event is acceptable)
+//e contains all the info about the event and since e is a object the properties can be accessed by obj.key
+//to know which key was press e.key will give the key pressed
+document.addEventListener('keydown', function (e) {
+  //return the key pressed in the event
+  console.log(e.key);
+
+  //it should work only if esc key is pressed and if the content is not hidden
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeButton();
+  }
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//we can elements by query selector and getElementById
+//when using getElementById we dont need to add # we can give the id name directly
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector();
+score0El.textContent = 0;
+score1El.textContent = 0;
