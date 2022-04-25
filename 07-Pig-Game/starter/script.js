3174cef93a6d4d8a2342b432fb06c54a6841e44f
Sudharsan <specsudharsan@gmail.com>
2022-04-25 17:08:04 +0530

    dice and cuureent score in progress

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -9,14 +9,35 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('btn--hold');
 
+//to change the content of the currentScore of the active player
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
+//have a separate variable for storing the current score rolled by the dice
+let currentScore = 0;
+
 //starting point
+//at the starting point change the total scores of both players to 0
 score0El.textContent = 0;
 score1El.textContent = 0;
+//change the dice visibility by adding the hidden class
 diceEl.classList.add('hidden');
 
+//when button is clicked to roll the dice add a eventlistener
 btnRoll.addEventListener('click', function () {
+  //changing the dice visibility back to normal by removing hidden class
   diceEl.classList.remove('hidden');
-  const diceRoll = Math.trunc(Math.random() * 6) + 1;
-  console.log(diceRoll);
-  document.querySelector('.dice').src = `dice-${diceRoll}.png`;
+  //create the dice number randomly
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // console.log(dice);
+
+  //change the dice image by using ${} notation according to the dice number
+  document.querySelector('.dice').src = `dice-${dice}.png`;
+  //if teh dice is not 1 then add the currentScore var with the dice value
+  if (dice !== 1) {
+    //todo:Need to change according to the current player
+    currentScore += dice;
+    //change the textcontent of the current score of the active player
+    currentScore0El.textContent = currentScore;
+  } else {
+  }
 });
