9d20b69349897fa5decfa6f920a3d7b7e1853b90
Sudharsan <specsudharsan@gmail.com>
2022-04-25 16:53:53 +0530

    dice is done

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,6 +6,17 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector();
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('btn--hold');
+
+//starting point
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  diceEl.classList.remove('hidden');
+  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceRoll);
+  document.querySelector('.dice').src = `dice-${diceRoll}.png`;
+});
